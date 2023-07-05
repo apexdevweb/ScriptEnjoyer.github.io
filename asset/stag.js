5e28387322a7d27561751b0c06080133e9e9e137
[apexdevweb] <apexdevweb@gmail.com>
2023-07-05 23:03:12 +0200

    version 1.0.7

---
+++ asset/stag.js
@@ -0,0 +1,11 @@
+function stagEl() {
+  anime({
+    targets: ".square",
+    scale: [
+      { value: 0.1, easing: "easeOutSine", duration: 500 },
+      { value: 1, easing: "easeInOutQuad", duration: 800 },
+    ],
+    delay: anime.stagger(200, { grid: [14, 5], from: "center" }),
+    direction: "alternate",
+  });
+}
