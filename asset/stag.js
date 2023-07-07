4734d7d7dc4d6c1e9ffbde11194de80d67d390d5
[apexdevweb] <apexdevweb@gmail.com>
2023-07-08 01:34:12 +0200

    version 1.2

--- asset/stag.js
+++ asset/stag.js
@@ -3,7 +3,7 @@ const stagEl = () => {
   anime({
     targets: ".square",
     scale: [
-      { value: 0.1, easing: "easeOutSine", duration: 500 },
+      { value: 0.1, easing: "easeOutSine", duration: 400 },
       { value: 1, easing: "easeInOutQuad", duration: 800 },
     ],
     delay: anime.stagger(200, { grid: [14, 5], from: "center" }),
