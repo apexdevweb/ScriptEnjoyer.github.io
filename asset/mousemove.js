4510d13fe458aa372a42ed91d34c34b33735539f
[apexdevweb] <apexdevweb@gmail.com>
2024-05-21 21:50:56 +0200

    Version 2.0

---
+++ asset/mousemove.js
@@ -0,0 +1,6 @@
+const mousemove = document.querySelector(".mouse_globe");
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
